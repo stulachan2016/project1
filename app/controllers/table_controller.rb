6e432e6aec2fb15b4e2389e6b7c1930af519d44a
Sandeep Tulachan <ktmnepali@gmail.com>
2016-05-08 23:00:32 -0700

    Adding term-project

---
+++ app/controllers/table_controller.rb
@@ -0,0 +1,42 @@
+class TableController < ApplicationController
+
+  # GET /table
+  # GET /posts.json
+
+  def index
+
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+   # @tables = Table.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    #@tables = Table.new(params)
+
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+
+  end
+end
